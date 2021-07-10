3d80015bfba92d69e278862e26c33fd6ede13d09
dweep119 <dweeppatel123@gmail.com>
2021-07-10 18:36:04 +0530

    Changes based on new UI on screen-1

--- src/views/Step1.js
+++ src/views/Step1.js
@@ -19,29 +19,29 @@ function Step1() {
       <div className="col-12 d-flex pr-0 pl-0">
         <div className="col-4 lh-22">
           <div className="overlap-group10">
-            <input className="w-100 rectangle-8 text-field-box border-1px-mist-gray" id="location" name="lastname"
-              placeholder="West Rogers Park" />
+            <div className="eventName">Event Name</div>
           </div>
-          <div className="d-flex mt-4">
-            <div className="address-1 roboto-medium-black-18px-22">Address:</div>
-            <div className="address-2 roboto-normal-black-18px-2 ml-3">6301 N. Western Ave<br />Chicago, IL 60659
+          <div className="d-flex mt-3">
+            <div className="address-1 roboto-medium-black-18px-22 col-3 text-right pl-0">Address:</div>
+            <div className="address-2 roboto-normal-black-18px-2">6301 N. Western Ave<br />Chicago, IL 60659
             </div>
           </div>
-
           <div className="d-flex mt-4">
-            <div className="text-3 roboto-medium-black-18px-22">Phone:<br />Email:<br />Website:</div>
-            <div className="text-4 roboto-normal-black-18px-2 ml-3">
-              (800)-325-1812<br />RogersPark@prism.org<br />prism.org</div>
+            <div className="text-3 roboto-medium-black-18px-22 col-3 text-right pl-0">Phone:<br />Email:<br />Website:</div>
+            <div className="text-4 roboto-normal-black-18px-2">
+              (800)-325-1812<br />info@prism.org<br />prism.org</div>
           </div>
-
           <div className="d-flex mt-4">
-            <div className="hours roboto-medium-black-18px-22">Hours:</div>
-            <div className="text-5 roboto-normal-black-18px-2 ml-3">
-              Monday<br />Tuesday<br />Wednesday<br />Thursday<br />Friday<br />Saturday<br />Sunday
+            <div className="hours roboto-medium-black-18px-22 col-3 text-right pl-0">Dates:</div>
+            <div className="text-5 roboto-normal-black-18px-2">
+              Monday, July 12<br />Tuesday, July 13<br />Wednesday, July 14<br />Thursday, July 15
             </div>
             <div className="text-6 roboto-normal-black-18px-2 ml-3">
-              Closed<br />1pm - 7pm<br />1pm - 7pm<br />1pm - 7pm<br />1pm - 7pm<br />1pm - 7pm<br />Closed
+              8-5pm<br />8-5pm<br />8-5pm<br />8-5pm
             </div>
+          </div>
+          <div className="btn-directions mt-4 col-6">
+            GET DIRECTIONS
           </div>
         </div>
         <div className="col-1"></div>
@@ -50,19 +50,30 @@ function Step1() {
             src="https://anima-uploads.s3.amazonaws.com/projects/60d0ce8207f6c272e04c5a8d/releases/60d36369024600c5e63507e4/img/rectangle-150@1x.png" alt="img" />
         </div>
       </div>
-      <div className="mt-5 mb-5 col-12 d-flex pr-0 pl-0">
-        <div className="overlap-group10 col-4 pl-0">
-          <label className="date roboto-bold-black-18px mr-1">Type of visit</label>
-          <input className="rectangle-8 border-1px-mercury" id="TypeOfVisit" name="lastname"
-            placeholder="Select type of visit" />
+      <div className="mt-5 mb-5">
+        <hr />
+      </div>
+      <div className="select-a-time-slot">Available Dates</div>
+      <div className="mt-2 mb-5 col-8 d-flex pr-0 pl-0">
+        <div className="col-3 pl-0">
+          <div className="btn-date">
+            July 12, 2021
+          </div>
         </div>
-        <div className="overlap-group10 col-4">
-          <label className="date roboto-bold-black-18px mr-1">Select a date</label>
-          <input className="rectangle-8 border-1px-mercury" id="Date" name="lastname" placeholder="Select Date" />
+        <div className="col-3 pl-0">
+          <div className="btn-date">
+            July 13, 2021
+          </div>
         </div>
-        <div className="overlap-group10 col-4">
-          <label className="date roboto-bold-black-18px mr-1">Date of Birth</label>
-          <input className="rectangle-8 border-1px-mercury" id="DOB" name="lastname" placeholder="Date of Birth" />
+        <div className="col-3 pl-0">
+          <div className="btn-date active">
+            July 14, 2021
+          </div>
+        </div>
+        <div className="col-3 pl-0">
+          <div className="btn-date">
+            July 15, 2021
+          </div>
         </div>
       </div>
       <div className="select-a-time-slot">Select a time slot</div>
